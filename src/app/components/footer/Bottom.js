e3addb568fd5e3ba32ec4273295273d0448e559c
vazhap519 <phankvelashvili.mail@gmail.com>
2026-03-24 09:21:01 +0400

    add settings socials

---
+++ src/app/components/footer/Bottom.js
@@ -0,0 +1,6 @@
+const Bottom = ({ copy }) => (
+  <div className="border-t border-gray-600 text-center py-4 text-sm text-gray-400">
+{copy ?? "© 2026 Safetech. ყველა უფლება დაცულია."}  </div>
+);
+
+export default Bottom;
