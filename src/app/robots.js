065a2703194b49c9817d7c647ae640a412581470
vazhap519 <phankvelashvili.mail@gmail.com>
2026-04-09 21:11:33 +0400

    sitemaps change

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,16 +1,13 @@
 export default function robots() {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
+  const baseUrl =
+    process.env.NEXT_PUBLIC_SITE_URL || "https://safetech.ge";
 
   return {
     rules: [
       {
         userAgent: "*",
         allow: "/",
-        disallow: [
-          "/admin",
-          "/api",
-          "/_next",
-        ],
+        disallow: ["/admin", "/api", "/_next"],
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
