8d967f6f45347e12d0a14fb22a6bee3a959c5464
vazha <phankvelashvili.mail@gmail.com>
2026-08-01 14:48:41 +0400

    Add service configurator listing page

---
+++ back/app/Filament/Resources/ServiceConfiguratorResource/Pages/ListServiceConfigurators.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ServiceConfiguratorResource\Pages;
+
+use App\Filament\Resources\ServiceConfiguratorResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListServiceConfigurators extends ListRecords
+{
+    protected static string $resource = ServiceConfiguratorResource::class;
+}
