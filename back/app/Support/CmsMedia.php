e9f1bf8c3a199da7bbc0785bebf152dfd78b5f6d
vazha <phankvelashvili.mail@gmail.com>
2026-08-02 16:30:42 +0400

    Add shared CMS media constraints

---
+++ back/app/Support/CmsMedia.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Support;
+
+final class CmsMedia
+{
+    public const MAX_FILE_SIZE_KB = 20 * 1024;
+
+    /** @var array<int, string> */
+    public const IMAGE_MIME_TYPES = [
+        'image/jpeg',
+        'image/png',
+        'image/webp',
+        'image/avif',
+    ];
+
+    private function __construct()
+    {
+    }
+}
