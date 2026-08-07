8bae1cbbca3a7fb2d981b454b006d468d288f5ee
vazha <phankvelashvili.mail@gmail.com>
2026-08-07 23:19:23 +0400

    Fix nested main landmark on review page

--- frontend/src/app/review/[token]/page.tsx
+++ frontend/src/app/review/[token]/page.tsx
@@ -18,8 +18,8 @@ export default async function ReviewRequestPage({
     const { token } = await params;
 
     return (
-        <main className="mx-auto min-h-[calc(100svh-8rem)] max-w-container-max px-5 pb-16 pt-28 sm:px-6 md:pb-20 lg:px-14 lg:pt-32">
+        <div className="mx-auto min-h-[calc(100svh-8rem)] max-w-container-max px-5 pb-16 pt-28 sm:px-6 md:pb-20 lg:px-14 lg:pt-32">
             <ReviewRequestForm token={token} />
-        </main>
+        </div>
     );
 }
