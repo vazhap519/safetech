f432dd678b4435e62698f973c1989c373e80d715
vazha <phankvelashvili.mail@gmail.com>
2026-08-07 16:22:05 +0400

    Centralize stable sitemap index paths

---
+++ frontend/src/lib/sitemap-index.js
@@ -0,0 +1,9 @@
+export const SITEMAP_INDEX_PATHS = [
+  "/sitemap-main.xml",
+  "/sitemap-services.xml",
+  "/sitemap-service-categories.xml",
+  "/sitemap-projects.xml",
+  "/sitemap-project-categories.xml",
+  "/sitemap-pages.xml",
+  "/sitemap-images.xml",
+];
