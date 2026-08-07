90093d33196efc495e3d5a9a0ed7ec8f6bbe437a
vazha <phankvelashvili.mail@gmail.com>
2026-08-07 18:44:15 +0400

    Add About page admin resource

---
+++ back/app/Filament/Resources/AboutPageResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AboutPageResource\Pages;
+use App\Filament\Support\AboutPageTranslationFields;
+use App\Filament\Support\NavigationGroup;
+use App\Models\SiteSetting;
+use Filament\Actions\EditAction;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class AboutPageResource extends Resource
+{
+    protected static ?string $model = SiteSetting::class;
+
+    protected static ?string $navigationLabel = 'ჩვენს შესახებ';
+
+    protected static ?string $modelLabel = 'ჩვენს შესახებ გვერდი';
+
+    protected static ?string $pluralModelLabel = 'ჩვენს შესახებ';
+
+    protected static string|\UnitEnum|null $navigationGroup = NavigationGroup::Pages;
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema->components([
+            ...AboutPageTranslationFields::sections(),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('key')
+                    ->label('გვერდი')
+                    ->formatStateUsing(fn (): string => 'ჩვენს შესახებ'),
+                TextColumn::make('updated_at')
+                    ->label('ბოლო განახლება')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->recordActions([
+                EditAction::make()->label('რედაქტირება'),
+            ]);
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->where('key', 'translations');
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAboutPageSettings::route('/'),
+            'edit' => Pages\EditAboutPageSetting::route('/{record}/edit'),
+        ];
+    }
+}
