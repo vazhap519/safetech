841e0a1d2cfc29f4e5240c8b8c7f8b74c33a1e6c
vazha <phankvelashvili.mail@gmail.com>
2026-08-10 10:47:30 +0400

    Add consultation modal close event

--- frontend/src/components/consultation/constants.ts
+++ frontend/src/components/consultation/constants.ts
@@ -1,2 +1,3 @@
 export const CONSULTATION_MODAL_ID = "consultation-modal";
 export const CONSULTATION_OPEN_EVENT = "safetech:consultation-open";
+export const CONSULTATION_CLOSE_EVENT = "safetech:consultation-close";
