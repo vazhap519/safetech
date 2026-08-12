59d1a7f68da4958ccafd8fc2185eeff760ee04a5
vazha <phankvelashvili.mail@gmail.com>
2026-08-12 19:42:34 +0400

    Add AI conversation list page

---
+++ back/app/Filament/Resources/AiConversationResource/Pages/ListAiConversations.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\AiConversationResource\Pages;
+
+use App\Filament\Resources\AiConversationResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListAiConversations extends ListRecords
+{
+    protected static string $resource = AiConversationResource::class;
+}
