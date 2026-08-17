b0c8f289ee4d97ffcbc249e84ed986e217087bc1
vazha <phankvelashvili.mail@gmail.com>
2026-08-17 22:46:22 +0400

    Fix service config while normalizing AI model

--- back/config/services.php
+++ back/config/services.php
@@ -16,7 +16,7 @@ return [
     | This file is for storing the credentials for third party services such
     | as Mailgun, Postmark, AWS and more. This file provides the de facto
     | location for this type of information, allowing packages to have a
-    | conventional file to locate this information.
+    | conventional file to locate the various service credentials.
     |
     */
 
@@ -36,7 +36,7 @@ return [
 
     'slack' => [
         'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
