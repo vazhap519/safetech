fed26a0717c1829ea53b07746748528ccf34ed70
vazha <phankvelashvili.mail@gmail.com>
2026-08-21 22:36:58 +0400

    Trigger social automation when projects are published

---
+++ back/app/Observers/ProjectSocialAutomationObserver.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Observers;
+
+use App\Jobs\PublishProjectToSocialAutomation;
+use App\Models\Project;
+
+class ProjectSocialAutomationObserver
+{
+    public function created(Project $project): void
+    {
+        $this->dispatchIfReady($project);
+    }
+
+    public function updated(Project $project): void
+    {
+        if (! $project->wasChanged(['is_published', 'published_at'])) {
+            return;
+        }
+
+        $this->dispatchIfReady($project);
+    }
+
+    private function dispatchIfReady(Project $project): void
+    {
+        if (! config('social_automation.project_publish.enabled', false)) {
+            return;
+        }
+
+        if (! $project->isReadyForSocialShare()) {
+            return;
+        }
+
+        PublishProjectToSocialAutomation::dispatch($project->getKey())->afterCommit();
+    }
+}
