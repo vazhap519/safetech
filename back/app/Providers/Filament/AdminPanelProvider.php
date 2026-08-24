ae35be667e934c78786956c2df6336383c50bfec
vazha <phankvelashvili.mail@gmail.com>
2026-08-25 03:15:48 +0400

    Enable admin lead notification center

--- back/app/Providers/Filament/AdminPanelProvider.php
+++ back/app/Providers/Filament/AdminPanelProvider.php
@@ -46,6 +46,8 @@ class AdminPanelProvider extends PanelProvider
             ->databaseTransactions()
             ->unsavedChangesAlerts()
             ->broadcasting(false)
+            ->databaseNotifications()
+            ->databaseNotificationsPolling('5s')
             ->navigationGroups(NavigationGroup::class)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
