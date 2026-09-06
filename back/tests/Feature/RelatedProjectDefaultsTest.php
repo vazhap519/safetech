b1f59eaf846b314fcd8e031a81328b7684edae75
vazha <phankvelashvili.mail@gmail.com>
2026-09-06 03:26:04 -0700

    Restore related project locale assertion

--- back/tests/Feature/RelatedProjectDefaultsTest.php
+++ back/tests/Feature/RelatedProjectDefaultsTest.php
@@ -155,7 +155,7 @@ class RelatedProjectDefaultsTest extends TestCase
         $this->getJson("/api/projects/{$current->slug}?locale=ru")
             ->assertOk()
             ->assertJsonPath('data.related.0.title', 'Russian project title')
-            ->assertJsonPath('data.related.0.category', 'Russian project category')
+            ->assertJsonPath('data.related.0.category', 'Russian category')
             ->assertJsonPath('data.related.0.imageAlt', 'Russian project image alt');
     }
 
