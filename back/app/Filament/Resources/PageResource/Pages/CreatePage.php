f801c8a5a69925c32782eac4a302e3b5610a8224
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:20:46 -0700

    Add AI page content generator to create form

--- back/app/Filament/Resources/PageResource/Pages/CreatePage.php
+++ back/app/Filament/Resources/PageResource/Pages/CreatePage.php
@@ -2,10 +2,20 @@
 
 namespace App\Filament\Resources\PageResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\PageResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreatePage extends CreateRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = PageResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            $this->aiContentAction(),
+        ];
+    }
 }
