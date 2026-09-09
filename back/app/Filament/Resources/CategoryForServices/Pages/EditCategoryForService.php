9af33c602f6fa8dc21a8ab6ae68488a7e8ecd677
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:24:46 -0700

    Add AI generator to service category edit form

--- back/app/Filament/Resources/CategoryForServices/Pages/EditCategoryForService.php
+++ back/app/Filament/Resources/CategoryForServices/Pages/EditCategoryForService.php
@@ -2,17 +2,21 @@
 
 namespace App\Filament\Resources\CategoryForServices\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\CategoryForServices\CategoryForServiceResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditCategoryForService extends EditRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = CategoryForServiceResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
+            $this->aiContentAction(),
             DeleteAction::make()
                 ->label('წაშლა')
                 ->requiresConfirmation(),
