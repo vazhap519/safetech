77aa8cd405d27c2b04257abe7f340966915afb67
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:20:52 -0700

    Add AI page content generator to edit form

--- back/app/Filament/Resources/PageResource/Pages/EditPage.php
+++ back/app/Filament/Resources/PageResource/Pages/EditPage.php
@@ -2,16 +2,22 @@
 
 namespace App\Filament\Resources\PageResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\PageResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditPage extends EditRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = PageResource::class;
 
     protected function getHeaderActions(): array
     {
-        return [DeleteAction::make()];
+        return [
+            $this->aiContentAction(),
+            DeleteAction::make(),
+        ];
     }
 }
