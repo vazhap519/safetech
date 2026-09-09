e1ba4f4c1672f2ff67b1d23c6a354516efcb577f
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:24:41 -0700

    Add AI generator to service category create form

--- back/app/Filament/Resources/CategoryForServices/Pages/CreateCategoryForService.php
+++ back/app/Filament/Resources/CategoryForServices/Pages/CreateCategoryForService.php
@@ -2,10 +2,20 @@
 
 namespace App\Filament\Resources\CategoryForServices\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\CategoryForServices\CategoryForServiceResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCategoryForService extends CreateRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = CategoryForServiceResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            $this->aiContentAction(),
+        ];
+    }
 }
