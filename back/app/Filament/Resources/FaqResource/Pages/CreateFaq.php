c73b153928f8d645c7bd9ce7e7c342b1dac50880
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:21:30 -0700

    Add AI FAQ content generator to create form

--- back/app/Filament/Resources/FaqResource/Pages/CreateFaq.php
+++ back/app/Filament/Resources/FaqResource/Pages/CreateFaq.php
@@ -2,10 +2,20 @@
 
 namespace App\Filament\Resources\FaqResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\FaqResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateFaq extends CreateRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = FaqResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            $this->aiContentAction(),
+        ];
+    }
 }
