7ba19549fcdbbbf8bd9c3fd5c30c093b097f8dfb
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:23:55 -0700

    Add AI generator to local service landing create form

--- back/app/Filament/Resources/LocalServiceLandingResource/Pages/CreateLocalServiceLanding.php
+++ back/app/Filament/Resources/LocalServiceLandingResource/Pages/CreateLocalServiceLanding.php
@@ -2,10 +2,20 @@
 
 namespace App\Filament\Resources\LocalServiceLandingResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\LocalServiceLandingResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateLocalServiceLanding extends CreateRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = LocalServiceLandingResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            $this->aiContentAction(),
+        ];
+    }
 }
