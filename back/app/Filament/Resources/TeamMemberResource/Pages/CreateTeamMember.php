0cbc4a1441f14fe4ea7f9c688eba0bac46d2205e
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:27:18 -0700

    Add AI generator to team member create form

--- back/app/Filament/Resources/TeamMemberResource/Pages/CreateTeamMember.php
+++ back/app/Filament/Resources/TeamMemberResource/Pages/CreateTeamMember.php
@@ -2,10 +2,20 @@
 
 namespace App\Filament\Resources\TeamMemberResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\TeamMemberResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateTeamMember extends CreateRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = TeamMemberResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            $this->aiContentAction(),
+        ];
+    }
 }
