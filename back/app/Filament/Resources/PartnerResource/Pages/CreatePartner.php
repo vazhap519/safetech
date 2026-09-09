1bc790c54a1e75b9b6b42f453174ac633400ee69
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:27:43 -0700

    Add AI generator to partner create form

--- back/app/Filament/Resources/PartnerResource/Pages/CreatePartner.php
+++ back/app/Filament/Resources/PartnerResource/Pages/CreatePartner.php
@@ -2,10 +2,20 @@
 
 namespace App\Filament\Resources\PartnerResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\PartnerResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreatePartner extends CreateRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = PartnerResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            $this->aiContentAction(),
+        ];
+    }
 }
