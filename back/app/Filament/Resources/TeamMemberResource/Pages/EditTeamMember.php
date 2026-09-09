171382672f02154476695ac4fff5c1b0c5064559
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:27:24 -0700

    Add AI generator to team member edit form

--- back/app/Filament/Resources/TeamMemberResource/Pages/EditTeamMember.php
+++ back/app/Filament/Resources/TeamMemberResource/Pages/EditTeamMember.php
@@ -2,16 +2,22 @@
 
 namespace App\Filament\Resources\TeamMemberResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\TeamMemberResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditTeamMember extends EditRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = TeamMemberResource::class;
 
     protected function getHeaderActions(): array
     {
-        return [DeleteAction::make()];
+        return [
+            $this->aiContentAction(),
+            DeleteAction::make(),
+        ];
     }
 }
