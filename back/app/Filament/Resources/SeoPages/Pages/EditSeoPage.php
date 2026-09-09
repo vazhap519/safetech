042ace8eea7d26c59c072f090299e9f8c3e30a70
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:21:53 -0700

    Add AI SEO page generator to edit form

--- back/app/Filament/Resources/SeoPages/Pages/EditSeoPage.php
+++ back/app/Filament/Resources/SeoPages/Pages/EditSeoPage.php
@@ -2,17 +2,21 @@
 
 namespace App\Filament\Resources\SeoPages\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\SeoPages\SeoPageResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditSeoPage extends EditRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = SeoPageResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
+            $this->aiContentAction(),
             DeleteAction::make(),
         ];
     }
