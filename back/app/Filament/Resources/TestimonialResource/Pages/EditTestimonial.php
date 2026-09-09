af10847bb2440430057f7d25dbeea4e67a40853e
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:27:37 -0700

    Add AI generator to testimonial edit form

--- back/app/Filament/Resources/TestimonialResource/Pages/EditTestimonial.php
+++ back/app/Filament/Resources/TestimonialResource/Pages/EditTestimonial.php
@@ -2,16 +2,22 @@
 
 namespace App\Filament\Resources\TestimonialResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\TestimonialResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditTestimonial extends EditRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = TestimonialResource::class;
 
     protected function getHeaderActions(): array
     {
-        return [DeleteAction::make()];
+        return [
+            $this->aiContentAction(),
+            DeleteAction::make(),
+        ];
     }
 }
