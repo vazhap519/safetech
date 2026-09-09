1a6390cf0cf9c163afaeb2f162897624ba2db6be
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:21:36 -0700

    Add AI FAQ content generator to edit form

--- back/app/Filament/Resources/FaqResource/Pages/EditFaq.php
+++ back/app/Filament/Resources/FaqResource/Pages/EditFaq.php
@@ -2,16 +2,22 @@
 
 namespace App\Filament\Resources\FaqResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\FaqResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditFaq extends EditRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = FaqResource::class;
 
     protected function getHeaderActions(): array
     {
-        return [DeleteAction::make()];
+        return [
+            $this->aiContentAction(),
+            DeleteAction::make(),
+        ];
     }
 }
