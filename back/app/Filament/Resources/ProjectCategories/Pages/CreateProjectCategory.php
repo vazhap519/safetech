9dccab27c13797b4c30fb7cd76c8b54b8ed83b64
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:24:21 -0700

    Add AI generator to project category create form

--- back/app/Filament/Resources/ProjectCategories/Pages/CreateProjectCategory.php
+++ back/app/Filament/Resources/ProjectCategories/Pages/CreateProjectCategory.php
@@ -2,10 +2,20 @@
 
 namespace App\Filament\Resources\ProjectCategories\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\ProjectCategories\ProjectCategoryResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateProjectCategory extends CreateRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = ProjectCategoryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            $this->aiContentAction(),
+        ];
+    }
 }
