be15412e06bb1b29931403bb4909618e303d3aa1
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:21:47 -0700

    Add AI SEO page generator to create form

--- back/app/Filament/Resources/SeoPages/Pages/CreateSeoPage.php
+++ back/app/Filament/Resources/SeoPages/Pages/CreateSeoPage.php
@@ -2,10 +2,20 @@
 
 namespace App\Filament\Resources\SeoPages\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\SeoPages\SeoPageResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateSeoPage extends CreateRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = SeoPageResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            $this->aiContentAction(),
+        ];
+    }
 }
