5cd5fe27e4278c6692147a42032901ba83ee1fe9
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:24:28 -0700

    Add AI generator to project category edit form

--- back/app/Filament/Resources/ProjectCategories/Pages/EditProjectCategory.php
+++ back/app/Filament/Resources/ProjectCategories/Pages/EditProjectCategory.php
@@ -2,17 +2,21 @@
 
 namespace App\Filament\Resources\ProjectCategories\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\ProjectCategories\ProjectCategoryResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditProjectCategory extends EditRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = ProjectCategoryResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
+            $this->aiContentAction(),
             DeleteAction::make(),
         ];
     }
