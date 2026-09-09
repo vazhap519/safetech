ef7d24bb5bafd18fd558b856d2785b178163aacf
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:24:03 -0700

    Add AI generator to local service landing edit form

--- back/app/Filament/Resources/LocalServiceLandingResource/Pages/EditLocalServiceLanding.php
+++ back/app/Filament/Resources/LocalServiceLandingResource/Pages/EditLocalServiceLanding.php
@@ -2,17 +2,21 @@
 
 namespace App\Filament\Resources\LocalServiceLandingResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\LocalServiceLandingResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditLocalServiceLanding extends EditRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = LocalServiceLandingResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
+            $this->aiContentAction(),
             DeleteAction::make()
                 ->label('წაშლა')
                 ->requiresConfirmation(),
