18c9ae6d7673f80167742ce653b4d1e47439ca8b
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:27:51 -0700

    Add AI generator to partner edit form

--- back/app/Filament/Resources/PartnerResource/Pages/EditPartner.php
+++ back/app/Filament/Resources/PartnerResource/Pages/EditPartner.php
@@ -2,16 +2,22 @@
 
 namespace App\Filament\Resources\PartnerResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\PartnerResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditPartner extends EditRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = PartnerResource::class;
 
     protected function getHeaderActions(): array
     {
-        return [DeleteAction::make()];
+        return [
+            $this->aiContentAction(),
+            DeleteAction::make(),
+        ];
     }
 }
