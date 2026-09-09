9125d5d49c9867cdb781b50fa7494a254a8c8e8d
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:20:33 -0700

    Add AI service content generator to edit form

--- back/app/Filament/Resources/ServiceResource/Pages/EditService.php
+++ back/app/Filament/Resources/ServiceResource/Pages/EditService.php
@@ -2,17 +2,21 @@
 
 namespace App\Filament\Resources\ServiceResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\ServiceResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditService extends EditRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = ServiceResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
+            $this->aiContentAction(),
             DeleteAction::make()
                 ->label('წაშლა')
                 ->requiresConfirmation(),
