b8fa8f166c6a724d8d2e3a96e7ce6e85b7df45f3
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:19:58 -0700

    Add AI project content generator to create form

--- back/app/Filament/Resources/ProjectResource/Pages/CreateProject.php
+++ back/app/Filament/Resources/ProjectResource/Pages/CreateProject.php
@@ -2,10 +2,20 @@
 
 namespace App\Filament\Resources\ProjectResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\ProjectResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateProject extends CreateRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = ProjectResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            $this->aiContentAction(),
+        ];
+    }
 }
