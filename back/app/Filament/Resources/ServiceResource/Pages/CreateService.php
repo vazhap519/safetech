a3cd411a55d369d76932520f61a20cb4c64c007a
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:20:27 -0700

    Add AI service content generator to create form

--- back/app/Filament/Resources/ServiceResource/Pages/CreateService.php
+++ back/app/Filament/Resources/ServiceResource/Pages/CreateService.php
@@ -2,10 +2,20 @@
 
 namespace App\Filament\Resources\ServiceResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\ServiceResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateService extends CreateRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = ServiceResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            $this->aiContentAction(),
+        ];
+    }
 }
