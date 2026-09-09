a9aea308c3ea240c6b6ee084e33f9b06ef1fe9be
vazha <phankvelashvili.mail@gmail.com>
2026-09-08 17:27:30 -0700

    Add AI generator to testimonial create form

--- back/app/Filament/Resources/TestimonialResource/Pages/CreateTestimonial.php
+++ back/app/Filament/Resources/TestimonialResource/Pages/CreateTestimonial.php
@@ -2,10 +2,20 @@
 
 namespace App\Filament\Resources\TestimonialResource\Pages;
 
+use App\Filament\Concerns\HasAiContentGenerator;
 use App\Filament\Resources\TestimonialResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateTestimonial extends CreateRecord
 {
+    use HasAiContentGenerator;
+
     protected static string $resource = TestimonialResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            $this->aiContentAction(),
+        ];
+    }
 }
