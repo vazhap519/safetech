ad4e7abe1149d1e4669d75ce181bd1b5ec0c16a8
vazha <phankvelashvili.mail@gmail.com>
2026-09-19 10:04:38 -0700

    feat(planner): persist private camera layouts and expose admin planning workspace

---
+++ back/app/Filament/Resources/CameraPlanResource/Pages/ListCameraPlans.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\CameraPlanResource\Pages;
+
+use App\Filament\Resources\CameraPlanResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListCameraPlans extends ListRecords
+{
+    protected static string $resource = CameraPlanResource::class;
+}
