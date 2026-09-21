6dd96b3d522df7960fb1545dfeb1402b512a4961
vazha <phankvelashvili.mail@gmail.com>
2026-09-20 22:46:00 -0700

    fix: import Laravel Queueable trait from Illuminate Bus

--- back/app/Jobs/SendCameraPlanNotification.php
+++ back/app/Jobs/SendCameraPlanNotification.php
@@ -8,7 +8,7 @@ use App\Notifications\NewCameraPlanNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\Queueable;
+use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Notification;
