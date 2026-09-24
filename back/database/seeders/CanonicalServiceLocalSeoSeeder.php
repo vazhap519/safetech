50fa5b95fe4b54d7cefd3764af92ed25e757db67
vazha <phankvelashvili.mail@gmail.com>
2026-09-24 08:04:48 -0700

    Add canonical service Local SEO seeder scaffold

---
+++ back/database/seeders/CanonicalServiceLocalSeoSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+final class CanonicalServiceLocalSeoSeeder extends Seeder
+{
+    public function run(): void
+    {
+    }
+}
